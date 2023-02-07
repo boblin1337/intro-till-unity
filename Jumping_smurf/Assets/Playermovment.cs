3b64e7fba1cc37d431c967ef7fcbaac2b4f2bf14
boblin1337 <bogren.hampus04@gmail.com>
2023-02-07 16:31:54 +0100

    mer nytt

---
+++ Jumping_smurf/Assets/Playermovment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovment : MonoBehaviour
+{
+
+    public Rigibody rb;
+    public int ForwardForce = 100;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
