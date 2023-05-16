ff51bf4772db77c2ba96decf37eaa4493cb60e16
boblin1337 <bogren.hampus04@gmail.com>
2023-05-16 16:52:03 +0200

    god bless amerikat

--- Jumping_smurf/Assets/Lava.cs
+++ Jumping_smurf/Assets/Lava.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Lava : MonoBehaviour
 {
+
+    
     
     void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("jumping_smurf");
+        
+        SceneManager.LoadScene("GameOverMeny");
+        
     }
 
     
